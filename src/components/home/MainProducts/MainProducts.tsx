8ad1549dd47ce5b331e6aff6a6d565a98410633b
Gerardo Castillo <gerrcass@gmail.com>
2023-12-27 12:18:56 -0400

    Manejo de estado de carga con el archivo loading.tsx 21/57

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,25 +1,40 @@
-// Shopify API reference docs:
-//https://shopify.dev/docs/api/admin-rest/2023-10/resources/product
+import Image from "next/image";
+import styles from "./MainProducts.module.sass";
 
 const getProducts = async () => {
-  const response = await fetch(
-    `${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`,
-    {
-      headers: new Headers({
-        "X-Shopify-Access-Token": process.env.SHOPIFY_API_KEY || "",
-      }),
-    }
-  );
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(
+      `${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`,
+      {
+        headers: new Headers({
+          "X-Shopify-Access-Token": process.env.SHOPIFY_API_KEY || "",
+        }),
+      }
+    );
+    const { products } = await response.json();
+    return products;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const MainProducts = async () => {
   const products = await getProducts();
-  console.log("🚧", products);
+
   return (
-    <section>
-      <p>Main Products</p>
+    <section className={styles.MainProducts}>
+      <h3>✨ New products released!</h3>
+      <div className={styles.MainProducts__grid}>
+        {products?.map((product) => {
+          const imageSrc = product.images[0].src;
+          return (
+            <article key={product.id}>
+              <p>{product.title}</p>
+              <Image src={imageSrc} fill alt={product.title} loading="eager" />
+            </article>
+          );
+        })}
+      </div>
     </section>
   );
 };
