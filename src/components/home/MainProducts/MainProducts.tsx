d84ac472d9ebda5821a4a67b18c81ce300129561
Gerardo Castillo <gerrcass@gmail.com>
2023-12-27 10:21:11 -0400

    Integración con tienda Shopify via API

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,5 +1,22 @@
-export const MainProducts = () => {
-  console.log("🚧 Server env access example: ", process.env.SHOPIFY_HOSTNAME);
+// Shopify API reference docs:
+//https://shopify.dev/docs/api/admin-rest/2023-10/resources/product
+
+const getProducts = async () => {
+  const response = await fetch(
+    `${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`,
+    {
+      headers: new Headers({
+        "X-Shopify-Access-Token": process.env.SHOPIFY_API_KEY || "",
+      }),
+    }
+  );
+  const data = await response.json();
+  return data;
+};
+
+export const MainProducts = async () => {
+  const products = await getProducts();
+  console.log("🚧", products);
   return (
     <section>
       <p>Main Products</p>
