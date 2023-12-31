501ceb49c10846dbc1e4e6cd27b545870d44e5e2
Gerardo Castillo <gerrcass@gmail.com>
2023-12-31 00:15:52 -0400

    Mejorando SEO de una página en Next.js 38/57

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,10 @@
 import { MainProducts } from "app/components/home/MainProducts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "✨ Future World ✨",
+  description: "Welcome to the future world, an E-commerce from other century",
+};
 
 export default function Home() {
   return (
--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,10 +1,29 @@
 import { redirect } from "next/navigation";
 import { ProductView } from "app/components/product/ProductView";
 import { getProducts } from "app/services/shopify/products";
+import sanitizeHtml from "sanitize-html";
 
 interface ProductPageProps {
   searchParams: {
     id: string;
+  };
+}
+
+export async function generateMetadata({ searchParams }: ProductPageProps) {
+  const id = searchParams.id;
+  const products = await getProducts(id);
+
+  return {
+    title: `${products[0].title} | Future World`,
+    description: sanitizeHtml(products[0].description, { allowedTags: [] }),
+    openGraph: {
+      images: [
+        {
+          url: products[0].image,
+          alt: products[0].title,
+        },
+      ],
+    },
   };
 }
 
