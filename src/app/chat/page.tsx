80c78e74e1f8938ac6c88c474b9591a97d4f5d1d
Gerardo Castillo <gerrcass@gmail.com>
2024-01-13 17:41:58 -0400

    Creando componente de chatbot de ventas con la SDK IA de Vercel 50/57

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,10 @@
+import { Chat } from "app/components/chat";
+
+export default function ChatPage() {
+  return (
+    <>
+      <h1 style={{ textAlign: "center" }}>Chatbot</h1>
+      <Chat />
+    </>
+  );
+}
