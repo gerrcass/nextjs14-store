35fca47a8d317dd83ae58e9ed2d1b90552b9423f
Gerardo Castillo <gerrcass@gmail.com>
2024-01-13 17:56:09 -0400

    Implementación de un bot de ventas en una app Next.js 51/57

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -3,18 +3,28 @@
 import { useChat } from "ai/react";
 import styles from "./Chat.module.sass";
 
-export const Chat = () => {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+export const Chat = (props: { agent: string }) => {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    initialMessages: [
+      {
+        id: "1",
+        role: "system",
+        content: props.agent,
+      },
+    ],
+  });
 
   return (
     <main className={styles.Chat}>
       <section className="mb-auto m">
-        {messages.map((m) => (
-          <div className="mb-4" key={m.id}>
-            {m.role === "user" ? "User: " : "AI: "}
-            {m.content}
-          </div>
-        ))}
+        {messages
+          .filter((m) => m.role !== "system")
+          .map((m) => (
+            <div className="mb-4" key={m.id}>
+              {m.role === "user" ? "User: " : "AI: "}
+              {m.content}
+            </div>
+          ))}
       </section>
       <form className="flex space-x-4" onSubmit={handleSubmit}>
         <input
